4b663af3010b41da1077b1e14f719891adbc2660
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-01-18 14:30:41 -0800

    add delete by id

--- sample_backend.py
+++ sample_backend.py
@@ -59,14 +59,26 @@ def get_users():
         return subdict
     return users"""
     
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return ({})
-    return users
+    if request.method == 'GET':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        return users
+    elif request.method == 'DELETE':
+        if id:
+            count = 0
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].pop(count)
+                    resp = jsonify(success=True)
+                    resp.status_code = 201
+                    return resp
+                count += 1
+        return jsonify(success=False)
 
 @app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
