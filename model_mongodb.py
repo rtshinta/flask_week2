5f644b1a129fa12f1056835223346657ab32df77
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2021-01-27 12:30:35 -0800

    connect backend to mongodb

--- model_mongodb.py
+++ model_mongodb.py
@@ -47,3 +47,9 @@ class User(Model):
         for user in users:
             user["_id"] = str(user["_id"])
         return users
+
+    def find_by_name_and_job(self, name, job):
+        users = list(self.collection.find({"name": name, "job":job}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
--- sample_backend.py
+++ sample_backend.py
@@ -3,122 +3,67 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS
 import random
+import string
+from model_mongodb import User
+
+
 app = Flask(__name__)
-CORS(app)
 
-users = { 
-   'users_list' :
-   [
-      { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-      },
-      {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-      },
-      {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-      }, 
-      {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-      },
-      {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-      }
-   ]
-}
+#CORS stands for Cross Origin Requests.
+CORS(app) #Here we'll allow requests coming from any domain. Not recommended for production environment.
 
-def randomID():
-    l1 = chr(random.randint(65, 122))
-    l2 = chr(random.randint(65, 122))
-    l3 = chr(random.randint(65, 122))
-    num = random.randint(100, 999)
-    return l1 + l2 + l3 + str(num)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return 'Hello, World!'
 
-"""@app.route('/users')
-def get_users():
-    search_username = request.args.get('name')
-    if search_username:
-        subdict = {'users_list' : []}
-        for user in users['users_list']:
-            if user['name'] == search_username:
-                subdict['users_list'].append(user)
-        return subdict
-    return users"""
-    
-@app.route('/users/<id>', methods=['GET', 'DELETE'])
-def get_user(id):
-    if request.method == 'GET':
-        if id:
-            for user in users['users_list']:
-                if user['id'] == id:
-                    return user
-            return ({})
-        return users
-    elif request.method == 'DELETE':
-        if id:
-            count = 0
-            for user in users['users_list']:
-                if user['id'] == id:
-                    users['users_list'].pop(count)
-                    resp = jsonify(success=True)
-                    resp.status_code = 201
-                    return resp
-                count += 1
-        return jsonify(success=False)
-
-@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+@app.route('/users', methods=['GET', 'POST'])
 def get_users():
     if request.method == 'GET':
         search_username = request.args.get('name')
         search_job = request.args.get('job')
         if search_username and search_job:
-            subdict = {'users_list' : []}
-            for user in users['users_list']:
-                if user['name'] == search_username and user['job'] == search_job:
-                    subdict['users_list'].append(user)
-            return subdict
-        if search_username:
-            subdict = {'users_list' : []}
-            for user in users['users_list']:
-                if user['name'] == search_username:
-                    subdict['users_list'].append(user)
-            return subdict
-        return users
+            # TODO: Replace with database access
+            #result = find_users_by_name_job(search_username, search_job)  
+            result = User().find_by_name_and_job(search_username, search_job)
+        elif search_username:
+            # using list shorthand for filtering the list.
+            # TODO: Replace with database access
+            #result = [user for user in users['users_list'] if user['name'] == search_username]
+            result = User().find_by_name(search_username)
+        else:
+            result = User().find_all()
+        return {"users_list": result}
     elif request.method == 'POST':
-        userToAdd = request.get_json()
-        userToAdd['id'] = randomID()
-        users['users_list'].append(userToAdd)
-        resp = jsonify(success=True)
-        resp.status_code = 201
-        #resp.status_code = 200 #optionally, you can always set a response code. 
-        # 200 is the default code for a normal response
+        userToAdd = request.get_json() # no need to generate an id ourselves
+        newUser = User(userToAdd)
+        newUser.save() # pymongo gives the record an "_id" field automatically
+        resp = jsonify(newUser), 201
         return resp
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+    if request.method == 'GET':
+        user = User({"_id":id})
+        if user.reload() :
+            return user
+        else :
+            return jsonify({"error": "User not found"}), 404
     elif request.method == 'DELETE':
-        userToDelete = request.args.get('name')
-        if userToDelete:
-            count = 0
-            for user in users['users_list']:
-                if user['name'] == userToDelete:
-                    users['users_list'].pop(count)
-                    resp = jsonify(success=True)
-                    resp.status_code = 201
-                    return resp
-                count += 1
-        return jsonify(success=False)
-    
+        user = User({"_id":id})
+        resp = user.remove()
 
+        # TODO: Check the resp object if the removal was successful or not.
+        # Return a 404 status code if it was not successful
+        #print(resp)
+        if resp['n'] == 1 and resp['ok'] == 1.0:
+            return resp, 201
+        return jsonify({"error": "User not found"}), 404
 
+"""def find_users_by_name_job(name, job):
+    subdict = {'users_list' : []}
+    for user in users['users_list']:
+        if user['name'] == name and user['job'] == job:
+            subdict['users_list'].append(user)
+    return subdict  """
